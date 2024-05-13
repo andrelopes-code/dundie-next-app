2a1e6dda56d9294ce7f8bc7a9b60ad0448cb43c8
André Lopes <andrelopes.code@gmail.com>
2024-05-13 11:57:10 +0000

    Rota para a criação de produtos

---
+++ src/app/api/admin/products/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse, NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { api } from "@/api/axios";
+
+export async function POST(request: NextRequest) {
+    // Verifica o token de autenticação
+    const access_token = cookies().get("access_token")?.value;
+    if (!access_token) {
+        return NextResponse.json({ detail: "Unauthorized" }, { status: 401 });
+    }
+
+    // Configura os headers da requisição
+    const config = {
+        headers: {
+            accept: "application/json",
+            authorization: `Bearer ${access_token}`,
+        },
+    };
+
+    const data = await request.json();
+
+    // Tenta realizar a requisição e retorna o resultado
+    try {
+        const res = await api.post("/admin/shop/product", data, config);
+        const response = NextResponse.json(res.data, { status: res.status });
+        return response;
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                detail:
+                    error?.response?.data?.detail ||
+                    "Error while creating product",
+            },
+            {
+                status: 404,
+            }
+        );
+    }
+}

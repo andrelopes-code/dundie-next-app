eb869fe9b72053e8bfe7335f567b2cbc3f45054a
André Lopes <andrelopes.code@gmail.com>
2024-05-13 23:14:43 +0000

    Adicionada a rota na api para a edição de produtos

--- src/app/api/admin/products/route.ts
+++ src/app/api/admin/products/route.ts
@@ -32,7 +32,43 @@ export async function POST(request: NextRequest) {
                     "Error while creating product",
             },
             {
-                status: 404,
+                status: error.response.status || 500,
+            }
+        );
+    }
+}
+
+export async function PATCH(request: NextRequest) {
+    // Verifica o token de autenticação
+    const access_token = cookies().get("access_token")?.value;
+    if (!access_token) {
+        return NextResponse.json({ detail: "Unauthorized" }, { status: 401 });
+    }
+
+    // Configura os headers da requisição
+    const config = {
+        headers: {
+            accept: "application/json",
+            authorization: `Bearer ${access_token}`,
+        },
+    };
+
+    const data = await request.json();
+
+    // Tenta realizar a requisição e retorna o resultado
+    try {
+        const res = await api.patch("/admin/shop/product", data, config);
+        const response = NextResponse.json(res.data, { status: res.status });
+        return response;
+    } catch (error: any) {
+        return NextResponse.json(
+            {
+                detail:
+                    error?.response?.data?.detail ||
+                    "Error while editing product",
+            },
+            {
+                status: error.response.status || 500,
             }
         );
     }
